513b05a53c9841f1c136b722a7b6cb882eeb2da0
Muhamed Youssry <mohamed.ahmed973@eng-st.cu.edu.eg>
2020-05-25 22:05:15 +0200

    first_commit_from_visual

--- max_pairwaise_product/max_pairwise_product1.cpp
+++ max_pairwaise_product/max_pairwise_product1.cpp
@@ -3,34 +3,56 @@
 
 using namespace std;
 
-long long MaxPairwiseProduct(vector<int> x , int length );
+long long MaxPairwiseProduct(vector<int> x, int length);
 
 int main()
 {
-    int len = 0 ;
+    int len = 0;
     cout << "Enter the vector length: ";
-    cin >> len ; 
+    cin >> len;
     vector<int> data(len);
     for (int i = 0; i < len; i++)
     {
         cin >> data[i];
         data.push_back(data[i]);
     }
-    cout << "the maximum pairwise product of this vector: " << MaxPairwiseProduct(data , len) << endl ; 
+    cout << "the maximum pairwise product of this vector: " << MaxPairwiseProduct(data, len) << endl;
+    cout << "the maximum pairwise product fast of this vector: " << MaxPairwiseProduct(data, len) << endl;
 }
 
-long long MaxPairwiseProduct(vector<int>x , int length )
+long long MaxPairwiseProduct(vector<int> x, int length)
 {
     long long result = 0;
-    for (int i = 0; i < length ; i++)
+    for (int i = 0; i < length; i++)
     {
-        for (int j = i+1; j < length ;j++)
+        for (int j = i + 1; j < length; j++)
         {
-           if (x[i]*x[j] > result )
-           {
-               result = x[i]*x[j]  ;
-           }
+            if (x[i] * x[j] > result)
+            {
+                result = x[i] * x[j];
+            }
         }
     }
     return result;
+}
+
+long long MaxPairwiseProductFast(vector<int> x, int length)
+{
+    int maxIndex1 = 0;
+    int maxIndex2 = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (x[i] > x[maxIndex1])
+        {
+            maxIndex1 = i ; 
+        }
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (x[i] > x[maxIndex2])
+        {
+            maxIndex2 = i ; 
+        }
+    }
+    return ( x[maxIndex1] *  x[maxIndex2] ) ;
 }
