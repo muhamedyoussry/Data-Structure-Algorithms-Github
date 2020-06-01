ebf775e9de7ba44dcaa8ea510643d5c5edd01f7f
Muhamed Youssry <mohamed.ahmed973@eng-st.cu.edu.eg>
2020-06-01 21:50:10 +0200

    add faster solution for fibonacci problem

--- fibonacci_number/README.md
+++ fibonacci_number/README.md
@@ -18,3 +18,30 @@
 
 #### what is recursive function ? 
 A recursive function is a function that calls itself during its execution. This enables the function to repeat itself several times, outputting the result and the end of each iteration.
+
+
+
+## Function 1: fastFibonacci
+
+* level: **Beginner**
+* implementation: **naive solution**
+
+### Problem
+ get the fibonacci function result faster
+
+### Algorithm
+ - make the user enter a number
+ - creat an empty array with the size of n numbers
+ - intialize array[0] = 0 and array[1] = 1
+ - make a loop from 2 to n 
+    - start to define array[i] = array[i-1] + array[i-2]
+- return array[n] as the last number of the array  
+
+ ### Go depper
+
+#### why using unsigned long long ? 
+To be able to store highly long and big integer positiver number 
+
+| Data type | Size | Range |
+|:---------------:| :---------: | :---------: |
+| unsigned long long | 8 bytes | 0 to +8,446,744,073,709,551,615 |
--- fibonacci_number/fibonacci.cpp
+++ fibonacci_number/fibonacci.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
 #include <cassert>
 
-using namespace std; 
+using namespace std;
 
-int naiveFibonacci (int n);
+unsigned long long naiveFibonacci(int n);
+unsigned long long fastFibonacci(int n);
 
-int main () {
-    int n = 0 ;
-    cout << "Enter n: " ;
-    cin >> n ; 
+int main()
+{
+    int n = 0;
+    cout << "Enter n: ";
+    cin >> n;
 
-    cout << naiveFibonacci(n) ;
+    cout << fastFibonacci(n) << endl;
+    cout << naiveFibonacci(n) << endl;
 }
 
-int naiveFibonacci (int n) {
-    if ( n <= 1 ) {
+unsigned long long naiveFibonacci(int n)
+{
+    if (n <= 1)
+    {
         // this means that n = 0 or n = 1
-        return n ;
+        return n;
     }
-    else if  ( n > 1 ){
+    else if (n > 1)
+    {
         // recursive function
-        return naiveFibonacci(n-1) + naiveFibonacci(n-2) ; 
+        return naiveFibonacci(n - 1) + naiveFibonacci(n - 2);
     }
-    else {
-        // to deal with worng user entry 
-        cout << "please make sure that you enter a number >= 0 " << endl ; 
+    else
+    {
+        // to deal with worng user entry
+        cout << "please make sure that you enter a number >= 0 " << endl;
     }
+}
+
+unsigned long long fastFibonacci(int n)
+{
+    // creat fibonacci array that holds n elements
+    int F[n] = {};
+    F[0] = 0;
+    F[1] = 1;
+
+    if (n > 1)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            F[i] = F[i - 1] + F[i - 2];
+        }
+    }
+    return F[n];
 }
